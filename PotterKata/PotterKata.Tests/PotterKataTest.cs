609619c5058e8e27e93c863f3413fdeb802d9281
Vernigora Sergei <dev.serg@gmail.com>
2015-03-20 13:33:21 +0300

    Small fix

---
+++ PotterKata/PotterKata.Tests/PotterKataTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PotterKata.Card.Concrete;
+using PotterKata.Card.Interface;
+
+namespace PotterKata.Tests
+{
+    public class PotterKataTest
+    {
+        protected ICard card;
+
+        [SetUp]
+        public void TestInit()
+        {
+            card = new BookCard();
+        }
+
+
+        [Test]
+        public void EmptyCard_Cost_Nothing()
+        {
+            var cost = card.GetTotal();
+            Assert.AreEqual(cost, 0);
+        }
+    }
+}
