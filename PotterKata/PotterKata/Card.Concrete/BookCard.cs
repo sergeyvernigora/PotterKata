aa1729a661e269ba96fec6dd60f34453b1ffd47a
Vernigora Sergei <dev.serg@gmail.com>
2015-03-20 14:09:42 +0300

    Calculate discounts on one copy of each book

--- PotterKata/PotterKata/Card.Concrete/BookCard.cs
+++ PotterKata/PotterKata/Card.Concrete/BookCard.cs
@@ -15,6 +15,21 @@ namespace PotterKata.Card.Concrete
     {
         private readonly List<IBook> _books;
 
+        private const int TwoBooks = 2;
+        private const int ThreeBooks = 3;
+        private const int ForeBooks = 4;
+        private const int FiveBooks = 5;
+        private const int SixBook = 6;
+        private const int SevenBooks = 7;
+
+        private const int Percent0 = 0;
+        private const int Percent5 = 5;
+        private const int Percent10 = 10;
+        private const int Percent15 = 15;
+        private const int Percent20 = 20;
+        private const int Percent25 = 25;
+        private const int Percent30 = 30;
+
         /// <summary>
         /// ctor
         /// </summary>
@@ -38,7 +53,40 @@ namespace PotterKata.Card.Concrete
         /// <returns></returns>
         public decimal GetTotal()
         {
-            return _books.Sum(x => x.Cost);
+            var total = _books.Sum(x => x.Cost);
+            var groups = _books.GroupBy(x => x.ID);
+            var discountPercent = GetDiscountPercent(groups.Count());
+
+            decimal discount = groups.Sum(@group => @group.First().Cost * discountPercent / 100);
+
+            return total - discount;
+        }
+
+
+        /// <summary>
+        /// Percent depends on 
+        /// </summary>
+        /// <param name="sameBooksCount"></param>
+        /// <returns></returns>
+        private int GetDiscountPercent(int sameBooksCount)
+        {
+            switch (sameBooksCount)
+            {
+                case TwoBooks:
+                    return Percent5;
+                case ThreeBooks:
+                    return Percent10;
+                case ForeBooks:
+                    return Percent15;
+                case FiveBooks:
+                    return Percent20;
+                case SixBook:
+                    return Percent25;
+                case SevenBooks:
+                    return Percent30;
+                default:
+                    return Percent0;
+            }
         }
     }
 }
